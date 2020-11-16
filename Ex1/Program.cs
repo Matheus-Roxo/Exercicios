4ac111e3661dd03762d74f2927b38d0031482501
Matheus Roxo Brandão <matheus.roxo.brandao31@gmail.com>
2020-11-16 18:53:17 -0300

    finalizados

---
+++ Ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meses, dias, horas, minutos;
+            
+            Console.WriteLine("Qual é a sua idade?");
+            int idade = int.Parse(Console.ReadLine());
+
+            meses = (idade*12);
+            dias = (idade*365);
+            horas = (idade*8760);
+            minutos = (idade*525600);
+            
+            Console.WriteLine("Você viveu durante "+meses+" Meses, "+dias+" dias, "+horas+" horas, "+minutos+" e minutos.");
+        }
+    }
+}
---
+++ Ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoNascimento, anoAtual, idade, semanas;
+
+            Console.WriteLine("Qual seu ano de nascimento?");
+            anoNascimento = int.Parse(Console.ReadLine());
+            Console.WriteLine("Em que ano você está?");
+            anoAtual = int.Parse(Console.ReadLine());
+
+            idade = (anoAtual-anoNascimento);
+            semanas = (idade*52);
+
+            Console.WriteLine("Você tem "+idade+" anos, que em semanas são "+semanas);
+        }
+    }
+}
